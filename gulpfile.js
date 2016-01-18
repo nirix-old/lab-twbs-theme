391f491621c5cb5519383d197ac5b7964db82a59
Jack Polgar <jack@polgar.id.au>
2016-01-18 14:47:54 +1100

    Add gulpfile.

---
+++ gulpfile.js
@@ -0,0 +1,69 @@
+var gulp       = require('gulp')
+    watch      = require('gulp-watch'),
+    sass       = require('gulp-sass'),
+    coffee     = require('gulp-coffee'),
+    concat     = require('gulp-concat'),
+    uglify     = require('gulp-uglify'),
+    sourcemaps = require('gulp-sourcemaps');
+
+var sassPaths = [
+    'node_modules',
+    'node_modules/bootstrap-sass/assets/stylesheets'
+];
+
+var beSassy = function() {
+    console.log('Being Sassy');
+
+    gulp.src([
+        './scss/lab.scss'
+    ])
+    .pipe(sourcemaps.init())
+    .pipe(sass({outputStyle: 'compressed', includePaths: sassPaths}).on('error', sass.logError))
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest('assets/css'));
+}
+
+var makeCoffee = function() {
+    console.log('Making coffee');
+
+    gulp.src('coffee/*.coffee')
+        .pipe(coffee())
+        .pipe(sourcemaps.init())
+        .pipe(uglify())
+        .pipe(sourcemaps.write('./'))
+        .pipe(gulp.dest('assets/js'));
+}
+
+gulp.task('compile', function(){
+    beSassy();
+    makeCoffee();
+    gulp.start('assets');
+});
+
+// Watch for changes
+gulp.task('watch', function(){
+    watch('scss/**/*.scss', beSassy);
+    watch('coffee/**/*.coffee', makeCoffee);
+});
+
+// Compile Sass
+gulp.task('sass', beSassy);
+
+// Compile CoffeeScript
+gulp.task('coffee', makeCoffee);
+
+// JavaScripts
+gulp.task('assets', function() {
+    gulp.src([
+        'node_modules/jquery/dist/jquery.js',
+        'node_modules/bootstrap-sass/assets/javascripts/bootstrap.js'
+    ])
+    .pipe(sourcemaps.init())
+    .pipe(uglify())
+    .pipe(concat('js.js'))
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest('assets/js'));
+
+    gulp.src(['node_modules/font-awesome/fonts/*'])
+        .pipe(gulp.dest('assets/fonts'));
+});
